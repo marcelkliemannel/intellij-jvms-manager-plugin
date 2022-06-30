a9d327ca0fdf979a4ceb90a68b396aaf0a220aef
Marcel Kliemannel <dev@marcelkliemannel.com>
2022-06-30 19:17:08 +0200

    Add refresh process information to GC notification

--- src/main/kotlin/dev/turingcomplete/intellijjpsplugin/ui/detail/ProcessDescriptionPanel.kt
+++ src/main/kotlin/dev/turingcomplete/intellijjpsplugin/ui/detail/ProcessDescriptionPanel.kt
@@ -2,25 +2,17 @@ package dev.turingcomplete.intellijjpsplugin.ui.detail
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.Task
-import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.Messages
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.util.ui.GridBag
 import com.intellij.util.ui.UIUtil
-import dev.turingcomplete.intellijjpsplugin.JpsPluginService
 import dev.turingcomplete.intellijjpsplugin.process.ProcessNode
-import dev.turingcomplete.intellijjpsplugin.ui.CommonsDataKeys
 import dev.turingcomplete.intellijjpsplugin.ui.JpsToolWindowFactory
-import dev.turingcomplete.intellijjpsplugin.ui.JvmProcessesMainPanel
 import dev.turingcomplete.intellijjpsplugin.ui.action.ForciblyTerminateProcessesAction
 import dev.turingcomplete.intellijjpsplugin.ui.action.GracefullyTerminateProcessesAction
+import dev.turingcomplete.intellijjpsplugin.ui.action.RefreshProcessInformationAction
 import dev.turingcomplete.intellijjpsplugin.ui.common.UiUtils
 import dev.turingcomplete.intellijjpsplugin.ui.common.overrideLeftInset
 import dev.turingcomplete.intellijjpsplugin.ui.common.overrideTopInset
@@ -80,7 +72,7 @@ class ProcessDescriptionPanel(val project: Project) : JBPanel<ProcessDescription
       add(GracefullyTerminateProcessesAction(collectJavaProcessesOnSuccess = false))
       add(ForciblyTerminateProcessesAction(collectJavaProcessesOnSuccess = false))
     }
-    val topActionGroup = DefaultActionGroup(processToolsGroup, RefreshAction())
+    val topActionGroup = DefaultActionGroup(processToolsGroup, RefreshProcessInformationAction(false))
 
     val myToolbar = ActionManager.getInstance().createActionToolbar("${JpsToolWindowFactory.TOOLBAR_PLACE_PREFIX}.toolbar.processDetails", topActionGroup, true)
     myToolbar.setTargetComponent(parent)
@@ -91,55 +83,4 @@ class ProcessDescriptionPanel(val project: Project) : JBPanel<ProcessDescription
   }
 
   // -- Inner Type -------------------------------------------------------------------------------------------------- //
-
-  private class RefreshAction : DumbAwareAction("Refresh Process Information", null, AllIcons.Actions.Refresh) {
-
-    var updateProcessInformationTaskRunning = false
-
-    override fun update(e: AnActionEvent) {
-      e.presentation.isEnabled = !updateProcessInformationTaskRunning
-    }
-
-    override fun actionPerformed(e: AnActionEvent) {
-      val processNode = CommonsDataKeys.getRequiredData(CommonsDataKeys.CURRENT_PROCESS_DETAILS_DATA_KEY, e.dataContext)
-
-      updateProcessInformationTaskRunning = true
-      UpdateProcessInformationTask(processNode,
-                                   e.project,
-                                   { e.project?.getService(JpsPluginService::class.java)?.processDetailsUpdated(listOf(processNode)) },
-                                   { updateProcessInformationTaskRunning = false })
-              .queue()
-    }
-  }
-
-  // -- Inner Type -------------------------------------------------------------------------------------------------- //
-
-  private class UpdateProcessInformationTask(val processNode: ProcessNode,
-                                             project: Project?,
-                                             val onSuccess: () -> Unit,
-                                             val onFinished: () -> Unit)
-    : Task.ConditionalModal(project, "Updating process information", false, DEAF) {
-
-    companion object {
-      private val LOG = Logger.getInstance(JvmProcessesMainPanel::class.java)
-    }
-
-    override fun run(indicator: ProgressIndicator) {
-      processNode.update()
-    }
-
-    override fun onThrowable(error: Throwable) {
-      val errorMessage = "Failed to update information of PID ${processNode.process.processID}"
-      LOG.warn(errorMessage, error)
-      Messages.showErrorDialog(project, "$errorMessage\n\nSee idea.log for more details.", "Updating Process Information Failed")
-    }
-
-    override fun onSuccess() {
-      onSuccess.invoke()
-    }
-
-    override fun onFinished() {
-      onFinished.invoke()
-    }
-  }
 }
--- src/main/kotlin/dev/turingcomplete/intellijjpsplugin/ui/detail/jvm/jvmaction/jtool/GarbageCollectorAction.kt
+++ src/main/kotlin/dev/turingcomplete/intellijjpsplugin/ui/detail/jvm/jvmaction/jtool/GarbageCollectorAction.kt
@@ -1,7 +1,9 @@
 package dev.turingcomplete.intellijjpsplugin.ui.detail.jvm.jvmaction.jtool
 
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.util.ui.UIUtil
+import dev.turingcomplete.intellijjpsplugin.ui.action.RefreshProcessInformationAction
 import dev.turingcomplete.intellijjpsplugin.ui.common.AnActionOptionButton
 import dev.turingcomplete.intellijjpsplugin.ui.common.NotificationUtils
 import dev.turingcomplete.intellijjpsplugin.ui.common.UiUtils
@@ -36,7 +38,9 @@ class GarbageCollectorAction : JvmAction("Garbage Collector") {
     val onSuccess: (String, JvmActionContext) -> Unit = { _, jvmActionContext ->
       NotificationUtils.notifyBalloon("Run Garbage Collection",
                                       "Garbage collection triggered on PID ${jvmActionContext.processNode.process.processID}.",
-                                      jvmActionContext.project)
+                                      jvmActionContext.project,
+                                      NotificationType.INFORMATION,
+                                      RefreshProcessInformationAction())
     }
     val commandLine: (JvmActionContext) -> Pair<JTool, List<String>> = {
       Pair(JTool.JCMD, listOf(it.processNode.process.processID.toString(), "GC.run"))
